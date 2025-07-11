09e6e901c4eda2b0065c1626879c0541e82d2a9b
MaharshiP343 <159413331+MaharshiP343@users.noreply.github.com>
2025-07-11 23:40:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,11 +1,5 @@
 class Solution:
-  def reverse(self, x: int) -> int:
-    ans = 0
-    sign = -1 if x < 0 else 1
-    x *= sign
-
-    while x:
-      ans = ans * 10 + x % 10
-      x //= 10
-
-    return 0 if ans < -2**31 or ans > 2**31 - 1 else sign * ans
+  def rotate(self, matrix: list[list[int]]) -> None:
+    matrix.reverse()
+    for i, j in itertools.combinations(range(len(matrix)), 2):
+      matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
